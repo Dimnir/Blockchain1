43a9c6f14f1d97388d634da92ff8bfaf7952179f
Dimnir <dimitrin@my.hit.ac.il>
2020-11-05 12:01:21 +0200

    .

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const {Blockchain,Block} = require('./Blockchain.js') // 
 
-let micaCoin = new Blockchain();
-micaCoin.addBlock(new Block(1,"2/11/2020",{amount:4})) // adds new block 
-micaCoin.addBlock(new Block(2,"2/11/2020",{amount:9}))
+let LiraCoin = new Blockchain();
+LiraCoin.addBlock(new Block(1,"2/11/2020",{amount:4})) // adds new block 
+LiraCoin.addBlock(new Block(2,"2/11/2020",{amount:9}))
 
-console.log('Blockchain valid ' + micaCoin.isChainValid()) // blockchain validation: "Blockchain valid T/F"
-console.log(JSON.stringify(micaCoin,null,4))
+console.log('Blockchain valid ' + LiraCoin.isChainValid()) // blockchain validation: "Blockchain valid T/F"
+console.log(JSON.stringify(LiraCoin,null,4))
 
 // changing a block will lead to a false validation
 // console.log('changing a block ...')
-// micaCoin.chain[1].data = {amount:69}
-// console.log('Blockchain valid ' + micaCoin.isChainValid())
+// LiraCoin.chain[1].data = {amount:69}
+// console.log('Blockchain valid ' + LiraCoin.isChainValid())
